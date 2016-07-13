ff60965c58ca47f95a54237b539002db8705b5ca
arjayLaygo <bimbotawski58@gmail.com>
2016-07-13 18:20:04 +0800

    Blingbling

---
+++ Team Kain. b with color.cpp
@@ -0,0 +1,36 @@
+    #include<iostream>
+	#include<windows.h>
+	using namespace std;
+	int main()
+	{
+		
+
+   {
+        HANDLE h = GetStdHandle (STD_OUTPUT_HANDLE);           
+        SetConsoleTextAttribute(h,FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+       
+		
+		
+		cout<<"\n\t\t\t\t\t:::COMPUTER PROGRAMMING 1:::"<<endl;
+        cout<<"\t\t\t\t\t\t ::CS-102::"<<endl;
+		cout<<"========================================================================================================================"<<endl;
+		
+		SetConsoleTextAttribute(h,FOREGROUND_RED | FOREGROUND_INTENSITY);
+	   
+		cout<<"\n\n";
+		cout<<"\t\t\t\t _______________________________________________"<<endl;
+		cout<<"\t\t\t\t|                Team Kaen :D                   |"<<endl;
+		cout<<"\t\t\t\t| #Donabelle Oprido (Pakaen Queen)              | "<<endl;
+		cout<<"\t\t\t\t| #Joshua Jamangal  (Kaen King)                 |"<<endl;
+		cout<<"\t\t\t\t| #Mark James Llanes(Busog Lusog)               |"<<endl;
+		cout<<"\t\t\t\t| #Arjay Laygo      (Busit Man)                 |"<<endl;
+		cout<<"\t\t\t\t|_______________________________________________|\n\n\n\n"<<endl;
+			
+	}
+	
+	
+	
+	system ("pause");
+	return 0;	
+	}
+        

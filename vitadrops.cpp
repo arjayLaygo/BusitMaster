083151316a3cdb3dc2cf45edcedf828990cd2d54
gelineEvangelista <gelineevangelista092@gmail.com>
2016-07-16 15:17:57 +0800

    Add files via upload

---
+++ vitadrops.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+int main(){
+
+HANDLE h = GetStdHandle (STD_OUTPUT_HANDLE);          
+SetConsoleTextAttribute(h,FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+
+int x;
+system("cls");
+for(x=1;x<2;x=x+2)
+{
+
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+Sleep(500);
+
+system("CLS");
+
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+Sleep(500);
+
+system("CLS");
+
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+Sleep(500);
+
+}
+
+
+
+for(x=1;x<2;x=x+1)
+{
+cout<<"                ________________"<<endl;
+cout<<"                \\              /"<<endl;
+cout<<"                 \\            /"<<endl;
+cout<<"                  \\          /"<<endl;
+cout<<"                   \\________/"<<endl;
+Sleep(500);
+}
+system("CLS");
+for(x=1;x<2;x=x+1)
+{
+    cout<<"                    "<<endl;
+	cout<<"                     "<<endl;
+	cout<<"                      "<<endl;
+	cout<<"                    <> "<<endl;
+	cout<<"                     \\ ||/"<<endl;
+	cout<<"                      \\||"<<endl;
+	cout<<"                _______||_______"<<endl;
+	cout<<"                \\              /"<<endl;
+	cout<<"                 \\            /"<<endl;
+	cout<<"                  \\          /"<<endl;
+	cout<<"                   \\________/"<<endl;
+    Sleep(800);
+}                   
+system("cls");
+for(x=1;x<2;x=x+2)
+{
+
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+Sleep(800);
+
+system("CLS");
+
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+Sleep(800);
+
+system("CLS");
+
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+cout<<"                       /\\ "<<endl;
+cout<<"                      (..)"<<endl;
+Sleep(800);
+
+}
+
+{
+cout<<"                ________________"<<endl;
+cout<<"                \\              /"<<endl;
+cout<<"                 \\            /"<<endl;
+cout<<"                  \\          /"<<endl;
+cout<<"                   \\________/"<<endl;
+Sleep(800);
+}
+system("CLS");
+for(x=1;x<2;x=x+1)
+{
+    cout<<"                     "<<endl;
+	cout<<"                       ||/  "<<endl;
+	cout<<"                       ||/"<<endl;
+	cout<<"                    <> ||"<<endl;
+	cout<<"                     \\ ||/"<<endl;
+	cout<<"                      \\||"<<endl;
+	cout<<"                _______||_______"<<endl;
+	cout<<"                \\              /"<<endl;
+	cout<<"                 \\            /"<<endl;
+	cout<<"                  \\          /"<<endl;
+	cout<<"                   \\________/"<<endl;
+	Sleep(800);
+}
+
+
+
+
+system("pause");
+return 0;
+}
+	      

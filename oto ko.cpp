37433cdb7e3f5560bd4860d6865ebd9f33e95d07
Llanesmarkjames <mrkjmsllanes@gmail.com>
2016-07-16 11:27:07 +0800

    Car revised color

---
+++ oto ko.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+int z;
+for(z=1;z<100;z=z+1)
+{
+	
+HANDLE h = GetStdHandle (STD_OUTPUT_HANDLE);           
+SetConsoleTextAttribute(h,FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                  ________________            "<<endl;
+cout<<"            _____/               /\\________  "<<endl;
+cout<<"           /----/               / /       /'  "<<endl;
+cout<<"          /----/_ _ _ _ _ _ _ _/ /   >>> //   "<<endl;
+cout<<"         /_____|____|____|_____\\/_______//   "<<endl;
+cout<<"          \\-----(O)__|__\\_____,(O)------'   "<<endl;
+Sleep(700);
+	
+           
+SetConsoleTextAttribute(h,FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+system("CLS");              
+cout<<"                       ________________            "<<endl;
+cout<<"                 _____/               /\\________  "<<endl;
+cout<<"                /----/               / /       /'  "<<endl;
+cout<<"               /----/_ _ _ _ _ _ _ _/ /   >>> //   "<<endl;
+cout<<"              /_____|____|____|_____\\/_______//   "<<endl;
+cout<<"               \\-----(O)__|__\\_____,(O)------'   "<<endl;
+Sleep(700);
+	
+
+SetConsoleTextAttribute(h,FOREGROUND_RED | FOREGROUND_INTENSITY);
+ system("CLS");
+cout<<"                            ________________            "<<endl;
+cout<<"                      _____/               /\\________  "<<endl;
+cout<<"                     /----/               / /       /'  "<<endl;
+cout<<"                    /----/_ _ _ _ _ _ _ _/ /   >>> //   "<<endl;
+cout<<"                   /_____|____|____|_____\\/_______//   "<<endl;
+cout<<"                    \\-----(O)__|__\\_____,(O)------'   "<<endl;
+Sleep(700);
+	
+SetConsoleTextAttribute(h,FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                 ________________                                    "<<endl;  //15 here !
+cout<<"                           _____/               /\\________                           "<<endl;
+cout<<"                          /----/               / /       /'                          "<<endl;
+cout<<"                         /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                        /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                         \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+	
+SetConsoleTextAttribute(h,FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                      ________________                                    "<<endl; //20 
+cout<<"                                _____/               /\\________                           "<<endl;
+cout<<"                               /----/               / /       /'                          "<<endl;
+cout<<"                              /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                             /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                              \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+	
+SetConsoleTextAttribute(h,FOREGROUND_RED | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                           ________________                                    "<<endl; //25
+cout<<"                                     _____/               /\\________                           "<<endl;
+cout<<"                                    /----/               / /       /'                          "<<endl;
+cout<<"                                   /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                                  /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                                   \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+	
+
+SetConsoleTextAttribute(h,FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                                ________________                                    "<<endl; //30
+cout<<"                                          _____/               /\\________                           "<<endl;
+cout<<"                                         /----/               / /       /'                          "<<endl;
+cout<<"                                        /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                                       /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                                        \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+
+
+SetConsoleTextAttribute(h,FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+system("CLS");              
+cout<<"                                                     ________________                                    "<<endl;   
+cout<<"                                               _____/               /\\________                           "<<endl;
+cout<<"                                              /----/               / /       /'                          "<<endl;
+cout<<"                                             /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                                            /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                                             \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+
+
+SetConsoleTextAttribute(h,FOREGROUND_RED | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                                         ________________                                    "<<endl; //25
+cout<<"                                                   _____/               /\\________                           "<<endl;
+cout<<"                                                  /----/               / /       /'                          "<<endl;
+cout<<"                                                 /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                                                /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                                                 \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+
+
+
+SetConsoleTextAttribute(h,FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                                                ________________                                    "<<endl; 
+cout<<"                                                          _____/               /\\________                           "<<endl;
+cout<<"                                                         /----/               / /       /'                          "<<endl;
+cout<<"                                                        /----/_ _ _ _ _ _ _ _/ /   >>> //                           "<<endl;
+cout<<"                                                       /_____|____|____|_____\\/_______//                            "<<endl;
+cout<<"                                                       \\-----(O)__|__\\_____,(O)------'                             "<<endl;
+Sleep(700);
+
+
+
+SetConsoleTextAttribute(h,FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                                                          ________________            "<<endl;
+cout<<"                                                                    _____/               /\\________  "<<endl;
+cout<<"                                                                   /----/               / /       /'  "<<endl;
+cout<<"                                                                  /----/_ _ _ _ _ _ _ _/ /   >>> //   "<<endl;
+cout<<"                                                                 /_____|____|____|_____\\/_______//   "<<endl;
+cout<<"                                                                 \\-----(O)__|__\\_____,(O)------'   "<<endl;
+Sleep(700);
+
+
+SetConsoleTextAttribute(h,FOREGROUND_RED | FOREGROUND_INTENSITY);
+system("CLS");
+cout<<"                                                                               ________________           "<<endl;
+cout<<"                                                                         _____/               /\\________ "<<endl;
+cout<<"                                                                        /----/               / /       /' "<<endl;
+cout<<"                                                                       /----/_ _ _ _ _ _ _ _/ /   >>> //  "<<endl;
+cout<<"                                                                      /_____|____|____|_____\\/_______//  "<<endl;
+cout<<"                                                                      \\-----(O)__|__\\_____,(O)------'   "<<endl;
+Sleep(700);
+
+
+
+
+
+}
+system ("pause");
+return 0;
+}
